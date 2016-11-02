343d6bc14af50c680061f9689bcaaf085345c384
b8356860 <ken13025@hotmail.com.tw>
2016-11-02 17:57:09 +0800

    fix the error of randomlize sampling method of train data

--- simple_neuralnet2.py
+++ simple_neuralnet2.py
@@ -7,6 +7,7 @@ Created on Thu Sep  8 01:37:04 2016
 import MyecgError
 import tensorflow as tf
 import numpy as np
+import 
 
 def add_layer(name, inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
@@ -64,8 +65,92 @@ def random_batch(example, label, batch_num=None):
     order = np.argsort(rand)
     return example[order[:batch_num]], label[order[:batch_num]]
 
-def random_sampling(amount, *args):
-    for count,thing in enumerate(arg):
+#def random_mix(amount, *args):
+#    """
+#    Mix data of several types by randomlize method
+#    parameter
+#    ---------
+#    amount : int
+#        the number of per type user want to mix in output data
+#    returns
+#    -------
+#    mixed_data : list
+#        target data after mixed
+#    remain_data : list
+#        data isn't in mixed_data
+#    """
+#    mixed_data = []
+#    remain_data = []
+#    for count,data in enumerate(args):
+#        cnt = 0
+#        temp_data = []
+#        temp_remain = []
+#        for i in range(len(data)):
+#            if np.random.random_integers(0,1)==1:
+#                temp_data.append(data[i])
+#                cnt += 1
+#            else:
+#                temp_remain.append(data[i])
+#            if cnt>=amount:
+#                temp_remain.extend(data[i+1:])
+#                break
+#            elif cnt>=(len(data)/2):
+#                temp_remain.extend(data[i+1:])
+#                while(True):
+#                    vacancy = amount-len(temp_data)
+#                    if vacancy>=len(temp_data):
+#                        temp_data.extend(temp_data)
+#                    elif vacancy<=0:
+#                        break
+#                    else:
+#                        temp_data.extend(temp_data[:vacancy])
+#                break
+#            if i==len(data)-1 and len(temp_data)<amount:
+#                for 
+#        mixed_data.extend(temp_data)
+#        remain_data.extend(temp_remain)
+#    return mixed_data, remain_data
+    
+def random_mix(amount, *args):
+    """
+    Mix data of several types by randomlize method
+    parameter
+    ---------
+    amount : int
+        the number of per type user want to mix in output data
+    returns
+    -------
+    mixed_data : list
+        target data after mixed
+    remain_data : list
+        data isn't in mixed_data
+    """
+    mixed_data = []
+    remain_data = []
+    for count,data in enumerate(args):
+        data = np.array(data)
+        temp_data = []
+        rand = np.random.random_sample(len(data))
+        sort_order = np.argsort(rand)
+        if (len(data)/2)==amount:
+            temp_data.extend(list(data[sort_order[:int(len(data)/2)]]))
+            remain_data.extend(list(data[sort_order[int(len(data)/2):]]))
+        elif (len(data)/2)>amount:
+            temp_data.extend(list(data[sort_order[:amount]]))
+            remain_data.extend(list(data[sort_order[amount:]]))
+        else:
+            temp_data.extend(list(data[sort_order[:int(len(data)/2)]]))
+            remain_data.extend(list(data[sort_order[int(len(data)/2):]]))         
+            while(True):
+                vacancy = amount-len(temp_data)
+                if vacancy>=len(temp_data):
+                    temp_data.extend(temp_data)
+                elif vacancy<=0:
+                    break
+                else:
+                    temp_data.extend(temp_data[:vacancy])
+        mixed_data.extend(temp_data)
+    return np.array(mixed_data), np.array(remain_data)
         
 #def  input_pipeline():
     
@@ -79,19 +164,21 @@ data_close_TP_pairs = list(np.loadtxt('ECG_learning_Data_close_TP_pairs.txt', de
 data_lawP = list(np.loadtxt('ECG_learning_Data_lawP.txt', delimiter=','))
 #data_noise = list(np.loadtxt('ECG_learning_Data_noise.txt', delimiter=','))
 
+train_data, test_data = random_mix(150, data_normal, data_STEMI, data_combined,
+                                   data_AF, data_close_TP_pairs, data_lawP)
 
 #train_data = np.array(data_normal[:150] + data_STEMI[:37])
 #test_data = np.array(data_normal[150:] + data_STEMI[37:])
-train_data = np.array(data_STEMI[:37]  + data_STEMI[:37] + data_STEMI[:37] + 
-        data_STEMI[:37] + data_normal[:150] + data_combined[:150] + data_AF[:50] + 
-        data_AF[:50] + data_AF[:50] + data_close_TP_pairs[:150] + data_lawP[:50] + 
-        data_lawP[:50] + data_lawP[:50])
+#train_data = np.array(data_STEMI[:37]  + data_STEMI[:37] + data_STEMI[:37] + 
+#        data_STEMI[:37] + data_normal[:150] + data_combined[:150] + data_AF[:50] + 
+#        data_AF[:50] + data_AF[:50] + data_close_TP_pairs[:150] + data_lawP[:50] + 
+#        data_lawP[:50] + data_lawP[:50])
 
 #test_data = np.array(data_STEMI[37:] + data_normal[150:] + data_combined[150:300])
 #train_data = np.array(data_STEMI[:37] + data_STEMI[:37] + data_STEMI[:37] + 
 #                        data_STEMI[:37] + data_normal[:150] + data_combined[:150])
-test_data = np.array(data_STEMI[37:]+ data_normal[150:] + data_combined[150:] + 
-                    data_close_TP_pairs[150:] + data_AF[50:] + data_lawP[50:])
+#test_data = np.array(data_STEMI[37:]+ data_normal[150:] + data_combined[150:] + 
+#                    data_close_TP_pairs[150:] + data_AF[50:] + data_lawP[50:])
 
 x_traindata = normalize(train_data[:,1:].astype('float32'))
 y_traindata = np.zeros([len(x_traindata),6]).astype('float32')
@@ -116,8 +203,8 @@ with tf.name_scope('Input') as scope:
     ys = tf.placeholder(tf.float32, [None, 6], name= 'Y_input')
 # add hidden layer
     
-#hidden_1 = add_layer('hidden1', xs, 800, 64, activation_function = tf.nn.softmax)
-#hidden_2 = add_layer('hidden2', hidden_1, 64, 12, activation_function = tf.nn.softmax)   
+#hidden_1 = add_layer('hidden1', xs, 800, 512, activation_function = tf.nn.softmax)
+#hidden_2 = add_layer('hidden2', hidden_1, 512, 128, activation_function = tf.nn.softmax)   
     
 prediction = add_layer('output', xs, 800, 6, activation_function = tf.nn.softmax)
 
@@ -128,7 +215,7 @@ with tf.name_scope('loss'):
     cross_entropy = -tf.reduce_mean(tf.reduce_sum(ys*tf.log(prediction), reduction_indices=[1]))
     tf.scalar_summary('cross_entropy', cross_entropy)
 with tf.name_scope('train'):
-    train_step = tf.train.GradientDescentOptimizer(0.2).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
 
 # important step
 init = tf.initialize_all_variables()
@@ -137,7 +224,9 @@ merged = tf.merge_all_summaries()
 writer = tf.train.SummaryWriter('logs/', sess.graph)
 sess.run(init)
 
-for i in range(1000):
+train_errors = []
+accuracys = []
+for i in range(5000):
     # training
 #    batch_xs = x_traindata
 #    batch_ys = y_train_data
@@ -146,11 +235,15 @@ for i in range(1000):
     if (i+1) % 10 == 0:
 #        result = sess.run(merged, feed_dict={xs:batch_xs,ys:batch_ys})
 #        writer.add_summary(result, i)
+        train_error = sess.run(cross_entropy, feed_dict={xs:batch_xs,ys:batch_ys})
         accuracy = compute_accuracy(x_testdata, y_testdata)
+        train_errors.append(train_error)
+        accuracys.append(accuracy)
         print('step {0}\naccuracy : {1:.3f}%'.format(i+1,accuracy*100))
-        print('cross_entropy : {0}'.format(sess.run(cross_entropy, feed_dict={xs:batch_xs,ys:batch_ys})))
+        print('cross_entropy : {0}'.format(loss))
     #        print(sess.run(prediction, feed_dict={xs:batch_xs}))
         if accuracy==1:
             break
+
 print('Mission complete')
 sess.close()
